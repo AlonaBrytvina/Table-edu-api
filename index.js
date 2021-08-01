c59019945b2dc3d71062f3f1f867763782a72b38
Alyona Brytvina <alyona.ivanova.kh@gmail.com>
2021-08-01 12:02:09 +0300

    attached logic to buttons

--- index.js
+++ index.js
@@ -1,25 +1,71 @@
+async function getResults(quantity) {
+  const response = await fetch(
+    `https://randomuser.me/api/?results=${quantity}`
+  );
+  const json = await response.json();
+  return json;
+}
+const elem = document.querySelector(".elem");
 
+document.querySelector(".buttonGet").onclick = function () {
+  const url = document.querySelector(".textButton").value;
+  console.log(url);
+  getResults(url).then((data) => {
+    console.log(data);
+    let arr = data.results;
+    createTable(elem, arr);
+  });
+};
 
-async function getResults(){
-const response = await fetch('https://randomuser.me/api/?results=20')
-const json = await response.json()
-return json
+document.querySelector(".buttonDelete").onclick = function () {
+  elem.innerHTML = "";
+};
+
+function createTable(parent, arr) {
+  const cols = 4;
+  const rows = arr.length;
+  const table = document.createElement("table");
+  const thead = document.createElement("thead");
+  const thr = document.createElement("tr");
+
+  for (let i = 0; i < cols; i++) {
+    const th = document.createElement("th");
+    thr.appendChild(th);
+  }
+
+  thead.appendChild(thr);
+  table.appendChild(thead);
+
+  const tbody = document.createElement("tbody");
+
+  for (let j = 0; j < rows; j++) {
+    const tr = document.createElement("tr");
+
+    for (let i = 0; i < cols; i++) {
+      const td = document.createElement("td");
+      tr.appendChild(td);
+    }
+
+    tbody.appendChild(tr);
+  }
+
+  table.appendChild(tbody);
+  parent.appendChild(table);
 }
 
-// getResults()
-// .then((data) => {
-//     console.log(data);
-//     let arr = data.results;
-//     console.log(arr)
-// })
+//      const table = document.createElement('table');
 
-document.querySelector('.buttonGet').onclick = function(){
-    const url = document.querySelector('.textButton').value
-    console.log(url)
-    getResults()
-    .then((data) => {
-        console.log(data);
-        let arr = data.results;
-        console.log(arr)
-    })
-}
+//      for (let i=0; i < rows; i++){
+//          const tr = document.createElement('tr')
+
+//         for (let j=0; j < cols; j++){
+//             const td = document.createElement('td')
+//             tr.appendChild(td);
+
+//         }
+
+//         table.appendChild(tr)
+//      }
+
+//      parent.appendChild(table)
+//  }
